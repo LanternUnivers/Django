a9fc08556c1be6128e5efbc6c07879487e7be6ce
youta <youta@LAPTOP-D4MIV3HT.>
2025-07-25 17:35:18 +0900

    Fix: adjust build script and settings for deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
-export DJANGO_SETTINGS_MODULE=bookproject.bookproject.settings
+export DJANGO_SETTINGS_MODULE=bookproject.settings
 cd /opt/render/project/src
 pip install -r requirements.txt
 python3 bookproject/manage.py collectstatic --no-input
